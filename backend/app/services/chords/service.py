d2ebf7a4ea39bd89c8b3a3d234622726e65b8d98
Tim Retzlaff <retzlaff-tim@gmx.de>
2024-12-21 23:22:36 +0100

    Remove diminished chords from chords route result.

--- backend/app/services/chords/service.py
+++ backend/app/services/chords/service.py
@@ -7,7 +7,12 @@ def get_chords_by_key(key: str) -> ChordsSchema:
 
     chords: list[tuple[str, str, str, str]] = get_chords(key)
 
+    final_chords: list[str] = list(map(lambda chord: chord[3], chords))
+
+    # remove diminished chords
+    final_chords = [x for x in final_chords if "°" not in x]
+
     return ChordsSchema(
-        chords=list(map(lambda chord: chord[3], chords)),
+        chords=final_chords,
         key=key,
     )
